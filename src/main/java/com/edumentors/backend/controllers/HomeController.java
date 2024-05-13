ea5cf666da110230c0c3278647f64398ffd086f5
Knightking-9274 <119316323+Knightking-9274@users.noreply.github.com>
2024-05-13 19:44:38 +0530

    Changed Handler Mehtod

--- src/main/java/com/edumentors/backend/controllers/HomeController.java
+++ src/main/java/com/edumentors/backend/controllers/HomeController.java
@@ -2,7 +2,9 @@ package com.edumentors.backend.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Controller
 public class HomeController {
@@ -10,6 +12,11 @@ public class HomeController {
     public String home(Model model){
         model.addAttribute("title","3SV-Edumentors & Consultants");
         return "index";
+    }
+
+    @RequestMapping("/signup")
+    public String signUp(){
+        return "signup";
     }
 
 }
