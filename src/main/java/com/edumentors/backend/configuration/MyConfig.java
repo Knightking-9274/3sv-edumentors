b41c732ea2ad02d2acd6325c3c45e99f61de1091
Knightking-9274 <119316323+Knightking-9274@users.noreply.github.com>
2024-05-13 18:13:48 +0530

    Adding Entity to database

---
+++ src/main/java/com/edumentors/backend/configuration/MyConfig.java
@@ -0,0 +1,9 @@
+package com.edumentors.backend.configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MyConfig {
+
+    
+}
--- src/main/java/com/edumentors/backend/controllers/UserController.java
+++ src/main/java/com/edumentors/backend/controllers/UserController.java
@@ -3,7 +3,10 @@ package com.edumentors.backend.controllers;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.edumentors.backend.dto.UserDTO;
+import com.edumentors.backend.entities.User;
+import com.edumentors.backend.repositories.UserRepository;
 import com.edumentors.backend.services.UserService;
+import com.edumentors.backend.services.implementations.UserServiceImplementation;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -11,22 +14,30 @@ import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-
 
 
 @RestController
 public class UserController {
 
     @Autowired
-    private UserService userService;
+    private UserRepository userRepository;
 
-    @RequestMapping("")
-    public ResponseEntity<UserDTO> createUser(@RequestBody UserDTO userDTO){
+    private UserServiceImplementation userServiceImplementation;
+
+    // @RequestMapping("")
+    // public ResponseEntity<UserDTO> registerUser(@RequestBody UserDTO userDTO){
         
-        UserDTO createUserDTO = this.userService.createUser(userDTO);
-        return new ResponseEntity<>(createUserDTO,HttpStatus.CREATED);
+    //     UserDTO createUserDTO = this.userService.createUser(userDTO);
+    //     return new ResponseEntity<>(createUserDTO,HttpStatus.CREATED);
+    // }
+    @PostMapping("/register")
+    public String registerUer(@ModelAttribute("userDTO") UserDTO userDTO){
+        User user = this.userServiceImplementation.dtoToUser(userDTO);
+        this.userRepository.save(user);
+        return "signup";
     }
     
     
