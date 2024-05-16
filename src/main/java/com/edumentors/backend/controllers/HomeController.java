09fe71c91ecd4351802659bc59d8d8b9bf533566
Knightking-9274 <119316323+Knightking-9274@users.noreply.github.com>
2024-05-16 18:48:00 +0530

    add validations to registeration form

--- src/main/java/com/edumentors/backend/controllers/HomeController.java
+++ src/main/java/com/edumentors/backend/controllers/HomeController.java
@@ -1,26 +1,66 @@
 package com.edumentors.backend.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.edumentors.backend.dto.UserDTO;
+import com.edumentors.backend.entities.User;
+import com.edumentors.backend.repositories.UserRepository;
+import com.edumentors.backend.services.implementations.UserServiceImplementation;
+
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
 
 
 @Controller
+@RequestMapping("/")
 public class HomeController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private UserServiceImplementation userServiceImplementation;
+
+    //for handling home page
     @RequestMapping("home")
     public String home(Model model){
         model.addAttribute("title","3SV-Edumentors & Consultants");
         return "index";
     }
 
+    //for rendering registeration/signup page
     @GetMapping("/signup")
     public String signUp(@ModelAttribute("userDTO") UserDTO userDTO){
        return "signup";
     }
+
+    //for registering the user
+    @PostMapping("/register")
+    public String registerUser(@Valid @ModelAttribute("userDTO") UserDTO userDTO,BindingResult bindingResult, HttpSession session){
+
+        if (bindingResult.hasErrors()) {
+            return "signup";
+        }
+        try{
+        User user = this.userServiceImplementation.dtoToUser(userDTO);
+        this.userRepository.save(user);
+        session.setAttribute("registered","Registered sucessfully!");
+        System.out.println(session.getAttribute("registered"));
+        return "signup";
+        }
+        catch(Exception e){
+            session.setAttribute("error","Something went wrong! "+e.getMessage());
+            return "signup";
+        }
+    }
+
+    
 
     @GetMapping("/login")
     public String logIn(@ModelAttribute("userDTO") UserDTO userDTO){
--- src/main/java/com/edumentors/backend/dto/UserDTO.java
+++ src/main/java/com/edumentors/backend/dto/UserDTO.java
@@ -1,5 +1,11 @@
 package com.edumentors.backend.dto;
 
+import jakarta.validation.constraints.AssertTrue;
+import jakarta.validation.constraints.Email;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
@@ -13,11 +19,37 @@ import lombok.Setter;
 @AllArgsConstructor
 public class UserDTO {
     private long id;
+
+    @NotBlank(message = "field cannot be blank!")
+    @Size(min = 2,max = 20,message = "characters range must be between 2-20!")
     private String fName;
+
+    @NotBlank(message = "field cannot be blank!")
+    @Size(min = 2,max = 20,message = "characters range must be between 2-20!")
     private String lName;
+
+    @NotBlank(message="provide city name!")
     private String city;
+
+    @Email(message="invalid email!")
     private String email;
+
+    @Size(min = 8, message = "Password must be at least 8 characters long")
+    @NotBlank
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$", 
+             message = "Password must contain at least one digit, one uppercase letter, one lowercase letter, one special character, and no whitespace")
     private String password;
+    
+    
+    @NotBlank(message="Please enter mobile number")
+    @Pattern(regexp="^(?:\\+?91)?[789]\\d{9}$", message="Invalid mobile number")
     private String contact;
+
+    
+    @NotBlank(message="please provide about details")
+    @Size(min=5, max=50, message="About details must be between 5 and 50 characters")
     private String about;
+
+    @AssertTrue(message="You must accecpt terms and condition!")
+    boolean terms;
 }
